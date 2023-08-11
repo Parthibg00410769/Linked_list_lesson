80be1cbb0df1187d0d0f6900e4248e3f0ff4f3fc
Parthibg00410769 <g00410769@atu.ie>
2023-08-11 14:38:49 +0100

    dLinked_list_end

--- Project1/doubly_linked_list.py
+++ Project1/doubly_linked_list.py
@@ -51,6 +51,47 @@ class doublyLL:
                 n = n.ref
             n.ref = new_node
             new_node.prev = n
+    def insert_after(self,data,x):
+        if self.head is None:
+            print("LL is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                if x==n.data:
+                    break
+                n = n.ref
+            if n is None:
+                print("Given Node is present in the LL")
+            else:
+                new_node = Node(data)
+                new_node.ref = n.ref
+                new_node.prev = n
+                if n.ref is not None:
+                    n.ref.prev = new_node   #this checks if the current node has a next node,this line updates the next nodes prev reference
+                n.ref = new_node
+
+    def insert_front(self,data,x):
+        if self.head is None:
+            print("LL is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                if x==n.data:
+                    break
+                n = n.ref
+            if n is None:
+                print("Given Node is present in the LL")
+            else:
+                new_node = Node(data)
+                new_node.prev = n.prev
+                new_node.ref = n
+                if n.prev is not None:
+                    n.prev.ref = new_node
+                else:
+                    self.head = new_node
+                n.pref = new_node
+
+
 
 ll2 = doublyLL()
 
@@ -59,5 +100,6 @@ ll2.insert_begin(10)
 ll2.insert_end(23)
 ll2.insert_end(100)
 ll2.insert_begin(199)
+ll2.insert_after(69,100)
 ll2.print_LL()
-ll2.print_LL_Reverse()
+#ll2.print_LL_Reverse()
