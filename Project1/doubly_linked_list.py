cea43fb881b55ab52e69a8fd40749c050207afc4
Parthibg00410769 <g00410769@atu.ie>
2023-08-11 14:07:26 +0100

    dLinked_list_end

--- Project1/doubly_linked_list.py
+++ Project1/doubly_linked_list.py
@@ -22,7 +22,7 @@ class doublyLL:
             print("Linked List is empty!")
         else:
             n = self.head
-            while n is not None:
+            while n.ref is not None:
                 n = n.ref
             while n is not None:
                 print(n.data, end=" ")
@@ -33,7 +33,7 @@ class doublyLL:
             self.head = new_node
         else:
             print("Linked list is not empty!")
-    def insert_begin(self,data,x):
+    def insert_begin(self,data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -41,7 +41,23 @@ class doublyLL:
             new_node.ref = self.head
             self.head.prev = new_node
             self.head = new_node
-
+    def insert_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+            new_node.prev = n
 
 ll2 = doublyLL()
+
+ll2.insert_end(50)
+ll2.insert_begin(10)
+ll2.insert_end(23)
+ll2.insert_end(100)
+ll2.insert_begin(199)
+ll2.print_LL()
 ll2.print_LL_Reverse()
